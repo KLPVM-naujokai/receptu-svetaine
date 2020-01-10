74643bd829ddcb69f9c656bf341e15e76281ff7e
alidike15 <alidike15@students.lcc.lt>
2020-01-10 11:58:42 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    #logo {
+        width: 15%; 
+        height: 15%;
+        display:inline-block;
+    }
+
+    #pav{
+        font-family: "Comic Sans MS", cursive, sans-serif;
+        font-size: 300%;
+        text-align: center;
+        display:inline-block;
+        vertical-align: 60%;
+    }
+    header{
+        border-style: double;
+    }
+</style>
+</head>
+
+<header>
+    <img id="logo" src="https://cdn.pixabay.com/photo/2015/08/19/02/27/restaurant-895428_1280.png" alt="logo">
+    <h1 id="pav">Mūsų receptai </h1> 
+</header>
+</html>
