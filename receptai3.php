73975b8fcdb45fc26cee9f4a70abc960651fc835
Ausra <alidike15@students.lcc.lt>
2020-01-10 14:08:04 +0200

    Update receptai3.php

--- receptai3.php
+++ receptai3.php
@@ -1,31 +1,30 @@
-<html>
-<body>
+<h2 id="pavad">Keptos krevetes</h2><br>
+<img id="img" src ="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10164.jpg" alt = "Keptos krevetes" width = "200" height = "180">
 
-<h2>Keptos krevetes</h2><br>
-<img src ="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10164.jpg" alt = "Keptos krevetes" width = "200" height = "180">
+<div class="grid-container">
+<div class="grid-item" id="info">
 <p><b>Paruosimo laikas:</b> apie 10min.<br>
 <b>Porciju skaicius:</b> 2</p>
+</div>
 
-<ol><b>Ingredientai:</b>
-  <li><em><text style="color:red;">Krevetes</text></em>, 300 gramu</li>
-  <li><em><text style="color:red;">Baltas vynas</text></em>, 100 mililitru</li>
-  <li><em><text style="color:red";>Pipirai</text></em>, 1 ziupsnelis</li>
+<div class="grid-item" id="visiingr">
+  <ol><b>Ingredientai:</b>
+  <li><em><text>Krevetes</text></em>, 300 gramu</li>
+  <li><em><text>Baltas vynas</text></em>, 100 mililitru</li>
+  <li><em><text>Pipirai</text></em>, 1 ziupsnelis</li>
 </ol>
+  </div>
 
-<ul><b>Gaminimo eiga:</b>
+<div class="grid-item" id="gam">
+  <ul><b>Gaminimo eiga:</b>
   <li>Krevetes 5 minutes pamirkyti vyne. Nuvarvinti.</li>
   <li>Sumauti ant iešmelių, pagardinti pipirais.</li>
   <li>Kepti ant įkaitusių anglių, po 3 minutes kiekvieną pusę.</li>
-</ul><hr><br>
+  </ul><br>
+  </div>
+</div>
+  <hr>
   
 <a href="receptai2.php">Ziureti praejusi recepta</a><br>
 <a href="receptai4.php">Sekantis receptas</a><br>
 <a href="index.php">Grizti i pradini puslapi</a>
-
-
-
-
-
-
-</body>
-</html>
