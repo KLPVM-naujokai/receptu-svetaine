14bfa7672239aa1d61eb8d892b8c2b912f65a1d1
justas7 <59725580+justas7@users.noreply.github.com>
2020-01-10 13:02:41 +0200

    Update receptai3.php

--- receptai3.php
+++ receptai3.php
@@ -4,7 +4,7 @@
 <h2>Keptos krevetes</h2><br>
 <img src ="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10164.jpg" alt = "Keptos krevetes" width = "200" height = "180">
 <p><b>Paruosimo laikas:</b> apie 10min.<br>
-<b>Porciju skaicius:</b> 2</p><hr>
+<b>Porciju skaicius:</b> 2</p>
 
 <ol><b>Ingredientai:</b>
   <li><em><text style="color:red;">Krevetes</text></em>, 300 gramu</li>
@@ -16,7 +16,11 @@
   <li>Krevetes 5 minutes pamirkyti vyne. Nuvarvinti.</li>
   <li>Sumauti ant iešmelių, pagardinti pipirais.</li>
   <li>Kepti ant įkaitusių anglių, po 3 minutes kiekvieną pusę.</li>
-</ul>
+</ul><hr><br>
+  
+<a href="receptai2.php">Grizti i praejusi recepta</a><br>
+<a href="receptai4.php">Sekantis receptas</a><br>
+<a href="index.php">Grizti i pradini puslapi</a>
 
 
 
