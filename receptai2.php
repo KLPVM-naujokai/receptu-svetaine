82677529a1e4a1de3dd323bae7854aa2d985536a
Ausra <alidike15@students.lcc.lt>
2020-01-10 14:01:13 +0200

    Update receptai2.php

--- receptai2.php
+++ receptai2.php
@@ -1,25 +1,34 @@
 <html>
 <body>
 
-<h2>Astrios krevetes grilyje</h2><br>
-<img src ="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10173.jpg" alt ="Astrios krevetes grilyje" width ="200" height = "170">
+<h2 id="pavad">Astrios krevetes grilyje</h2><br>
+<img id="img" src ="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10173.jpg" alt ="Astrios krevetes grilyje" width ="200" height = "170">
+
+<div class="grid-container">
+<div class="grid-item" id="info">
 <p><b>Paruosimo laikas:</b> apie 5min.<br>
 <b>Porciju skaicius:</b> 4</p>
+</div>
 
-<ol><b>Ingredientai:</b>
+<div class="grid-item" id="visiingr">
+  <ol><b>Ingredientai:</b>
   <li><em><text style="color:red;">Krevetes</text></em>, 400 gramu</li>
   <li><em><text style="color:red;">Tabasko padazas</text></em>, 5 valgomieji saukstai</li>
   <li><em><text style="color:red;">Aitrioji paprika</text></em>, 0.5 arbatinio saukstelio</li>
   <li><em><text style="color:red;">Zalioji citrina</text></em>, 1 vienetas</li>
 </ol>
+ </div>
 
-<ul><b>Gaminimo eiga:</b>
+<div class="grid-item" id="gam">
+  <ul><b>Gaminimo eiga:</b>
   <li>Jei krevetės šaldytos, atitirpinti. Nusausinti.</li>
   <li>Krevetes apiberti čili, apšlakstyti padažu.</li>
   <li>Kepti grilyje po 2 minutes iš kiekvienos pusės.</li>
   <li>Patiekti su žaliosios citrinos skiltele</li>
-</ul><hr><br>
-  
+</ul><br>
+</div>
+</div>
+  <hr>
 <a href="receptai1.php">Ziureti praejusi recepta</a><br>
 <a href="receptai3.php">Sekantis receptas</a><br>
 <a href="index.php">Grizti i pradini puslapi</a>
