0c6590c180130400443b8f39a30700334928e386
Ausra <alidike15@students.lcc.lt>
2020-01-10 13:56:06 +0200

    Update receptai1.php

--- receptai1.php
+++ receptai1.php
@@ -1,31 +1,35 @@
 <html>
 <body>
 
-<h2>Kreveciu blyneliai</h2><br>
-<img src="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10229.jpg" alt ="Kreveciu blyneliai" width ="200", height ="150">
-<p><b>Paruosimo laikas:</b> apie 20min.<br>
-<b>Porciju skaicius:</b> 2</p>
+<h2 id="pavad">Krevečių blyneliai</h2><br>
+<img id="img" src="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10229.jpg" alt ="Kreveciu blyneliai" width ="200", height ="150">
 
+<div class="grid-container">
+<div class="grid-item" id="info">
+<p><b>Paruošimo laikas:</b> apie 20min.<br>
+<b>Porcijų skaicius:</b> 2</p>
+</div>
+
+<div class="grid-item" id="visiingr">
 <ol><b>Ingredientai:</b>
-  <li><em><text style="color:red;">Krevetes</text></em>, 200 gramu</li>
-  <li><em><text style="color:red;">Miltai</text></em>, 2 valgomieji saukstai</li>
-  <li><em><text style="color:red;">Aitriosios paprikos milteliai</text></em>, 1 ziupsnelis</li>
+  <li><em><text id="ingr">Krevetes</text></em>, 200 gramu</li>
+  <li><em><text id="ingr">Miltai</text></em>, 2 valgomieji saukstai</li>
+  <li><em><text id="ingr">Aitriosios paprikos milteliai</text></em>, 1 ziupsnelis</li>
 </ol>
+</div>
 
-
-
+<div class="grid-item" id="gam">
 <ul><b>Gaminimo eiga:</b>
   <li>Krevetes sutrinti kombainu, sumaišyti su miltais ir čili.</li>
   <li>Blynelius kepti aliejuje.</li>
   <li>Sudėti ant ranksluostinio popieriaus, kad nuvarvėtų riebalai.</li>
   <li>PaPatiekti su žalumynais ir citrina</li>
-</ul><hr><br>
-  
+</ul><br>
+</div>
+</div>
+<hr>
 <a href="receptai2.php">Sekantis receptas</a><br>
 <a href="index.php">Grizti i pradini puslapi</a>
-
-
-
 
 </body>
 </html>
