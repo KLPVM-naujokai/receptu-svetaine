439ea4aa4cf0ac7c19301cb848cbfa71935aa752
justas7 <59725580+justas7@users.noreply.github.com>
2020-01-10 13:03:43 +0200

    Update receptai4.php

--- receptai4.php
+++ receptai4.php
@@ -4,7 +4,7 @@
 <h2>Krevetes sonines patale</h2><br>
 <img src="https://www.receptai.lt/uploads/modules/recipes/fullsize/10197.jpg" alt = "Krevetes sonines patale" width = "200" height = "170">
 <p><b>Paruosimo laikas:</b> apie 20 min.<br>
-<b>Porciju skaicius:</b> 5</p><hr>
+<b>Porciju skaicius:</b> 5</p>
 
 <ol><b>Ingredientai:</b>
   <li><em><text style="color:red;">Krevetes</text></em>, 20 vienetu</li>
@@ -17,7 +17,11 @@
   <li>Kiekviena juostelės puse apvynioti po krevetę.</li>
   <li>Kepti grilyje iš abiejų pusių, kol šoninė pasidarys traški.</li>
   <li>Skanauti su klevų sirupu.</li>
-</ul>
+</ul><hr><br>
+  
+<a href="receptai3.php">Grizti i praejusi recepta</a><br>
+<a href="index.php">Grizti i pradini puslapi</a>
+
 
 </body>
 </html>
