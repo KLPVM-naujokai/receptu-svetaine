1173f174bef49204cc0411903ed76b763ba39931
Ausra <alidike15@students.lcc.lt>
2020-01-10 13:33:52 +0200

    Update header.php

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 </head>
 
 <header>
-    <img id="logo" src="https://cdn.pixabay.com/photo/2015/08/19/02/27/restaurant-895428_1280.png" alt="logo">
+    <a href="index.php"><img id="logo" src="https://cdn.pixabay.com/photo/2015/08/19/02/27/restaurant-895428_1280.png" alt="logo"></a>
     <h1 id="pav">Mūsų receptai </h1> 
 </header>
 </html>
