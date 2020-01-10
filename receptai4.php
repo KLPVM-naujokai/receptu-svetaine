5ad1f7995c44d3063ef3bdab6f5f5641a32861a3
Ausra <alidike15@students.lcc.lt>
2020-01-10 14:11:28 +0200

    Update receptai4.php

--- receptai4.php
+++ receptai4.php
@@ -1,27 +1,31 @@
-<html>
-<body>
+<h2 id="pavad">Krevetes sonines patale</h2><br>
+<img id="img" src="https://www.receptai.lt/uploads/modules/recipes/fullsize/10197.jpg" alt = "Krevetes sonines patale" width = "200" height = "170">
 
-<h2>Krevetes sonines patale</h2><br>
-<img src="https://www.receptai.lt/uploads/modules/recipes/fullsize/10197.jpg" alt = "Krevetes sonines patale" width = "200" height = "170">
+<div class="grid-container">
+<div class="grid-item" id="info">
 <p><b>Paruosimo laikas:</b> apie 20 min.<br>
 <b>Porciju skaicius:</b> 5</p>
+</div>
 
+<div class="grid-item" id="visiingr">
 <ol><b>Ingredientai:</b>
-  <li><em><text style="color:red;">Krevetes</text></em>, 20 vienetu</li>
-  <li><em><text style="color:red;">Saltai rukyte sonine</text></em>, 10 riekiu</li>
-  <li><em><text style="color:red;">Kotanyi zuvies kepsniu prieskoniai</text></em>, 1 ziupsnelis</li>
+  <li><em><text>Krevetes</text></em>, 20 vienetu</li>
+  <li><em><text>Saltai rukyte sonine</text></em>, 10 riekiu</li>
+  <li><em><text>Kotanyi zuvies kepsniu prieskoniai</text></em>, 1 ziupsnelis</li>
 </ol>
+  </div>
 
-<ul><b>Gaminimo eiga:</b>
+<div class="grid-item" id="gam">
+  <ul><b>Gaminimo eiga:</b>
   <li>Šoninės juosteles perpjauti perpus, įtrinti prieskoniais.</li>
   <li>Kiekviena juostelės puse apvynioti po krevetę.</li>
   <li>Kepti grilyje iš abiejų pusių, kol šoninė pasidarys traški.</li>
   <li>Skanauti su klevų sirupu.</li>
-</ul><hr><br>
+</ul><br>
+  </div>
+</div>
+  <hr>
   
 <a href="receptai3.php">Ziureti praejusi recepta</a><br>
 <a href="index.php">Grizti i pradini puslapi</a>
 
-
-</body>
-</html>
