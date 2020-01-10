06bbcf8f0299fb9981c50409321cec15996f5926
Georgita <46970959+Georgita@users.noreply.github.com>
2020-01-10 13:31:24 +0200

    Create receptai1_trumpas.php

---
+++ receptai1_trumpas.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<h2>Kreveciu blyneliai</h2><br>
+<img src="https://www.receptai.lt/uploads/modules/recipes/thumb380x250/10229.jpg" alt ="Kreveciu blyneliai" width ="200", height ="150">
+
+
+
+</body>
+</html>
