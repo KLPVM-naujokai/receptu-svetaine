09cabd39223c0ae62cba942fccd7af2115e70f5a
Georgita <46970959+Georgita@users.noreply.github.com>
2020-01-10 11:31:59 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: blue;
+   color: yellow;
+   text-align: left;
+}
+</style>
+</head>
+<body>
+
+<h2>Fixed/Sticky Footer Example</h2>
+<p>The footer is placed at the bottom of the page.</p>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+</body>
+</html> 
+?>
